98add08cc1cf442d6a5025458950f9a22705c905
Tesya Indriani <tesyahilal@gmail.com>
2025-12-03 02:12:56 +0800

    Update include.js

--- js/include.js
+++ js/include.js
@@ -1,4 +1,23 @@
 // Load Navbar
+
+// Detect base URL otomatis untuk GitHub Pages
+const BASE = window.location.pathname.replace(/\/[^/]*$/, "");
+
+// Load Navbar
+fetch(`${BASE}/navbar.html`)
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById("navbar-placeholder").innerHTML = data;
+    });
+
+// Load Footer
+fetch(`${BASE}/footer.html`)
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById("footer-placeholder").innerHTML = data;
+    });
+
+
 // fetch("/components/navbar.html")
 //   .then(res => res.text())
 //   .then(data => {
@@ -12,18 +31,18 @@
 //       document.getElementById("footer-placeholder").innerHTML = data;
 //   });
 
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("navbar.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("navbar-placeholder").innerHTML = data;
-        });
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//     fetch("navbar.html")
+//         .then(response => response.text())
+//         .then(data => {
+//             document.getElementById("navbar-placeholder").innerHTML = data;
+//         });
+// });
 
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("footer.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("navbar-placeholder").innerHTML = data;
-        });
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//     fetch("footer.html")
+//         .then(response => response.text())
+//         .then(data => {
+//             document.getElementById("navbar-placeholder").innerHTML = data;
+//         });
+// });
